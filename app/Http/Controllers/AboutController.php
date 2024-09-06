3013346631f144cc430b2a24dc1ffdda92df69b5
Fida <fidaacahya@gmail.com>
2024-09-06 10:45:54 +0700

    Praktikum 1-2

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about () {
+        return 'Nama saya Fida Cahya dan NIM saya 2241760001';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticlesController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,69 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//PAKTIKUM 1
+// no1
+// Route::get ('/hello', function () {
+//     return 'Hello World';
+// });
+
+//no2
+Route::get('/world', function () {
+    return 'world';
+});
+
+//Tugas1
+Route::get('/salam', function () {
+    return 'Selamat Datang';
+});
+
+//Tugas2
+Route::get('/about', function () {
+    return 'Nama saya Fida Cahya dengan NIM 2241760001';
+});
+
+//
+//no8
+// Route::get('/user/{name}', function ($name) {
+//     return 'Nama Saya '. $name;
+// });
+
+//no11
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke- '. $postId."Komentar ke-: ".$commentId;
+});
+
+//no 13
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID '. $id;
+});
+
+//no 14 (nama tidak muncul)
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama Saya '. $name;
+});
+
+// no17 (muncul nama saya fida karena memanggil yg no 14 'http://localhost/PWL_2024/public/user/fida')
+// Route::get('/user/{name?}', function ($name='John') {
+//     return 'Nama Saya '. $name;
+// });
+
+//no 18 (muncul nama john dengan url 'http://localhost/PWL_2024/public/user' )
+//Route::get('/user/{name?}', function ($name='John') {
+    //return 'Nama Saya '. $name;
+//});
+
+//PRAKTIKUM 2
+//no 4
+Route::get('/hello', [WelcomeController::class,'hello']);
+//no 6
+Route::get('/', [HomeController::class,'index']);
+Route::get('/about', [AboutController::class,'about']);
+Route::get('/articles', [ArticlesController::class,'articles']);
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
